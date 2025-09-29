30c95cdb17814422ba1ba91768bb9a719c0449df
sirasaki-konoha <rock-db@outlook.jp>
2025-09-29 17:09:19 +0900

    initialize

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if ! which mkarchiso 2>&1 >/dev/null; then
+	sudo pacman -S archiso
+fi
+
+[[ -e /tmp/archiso-tmp ]] && rm -rfv /tmp/archiso-tmp/
+
+sudo mkarchiso -v -w /tmp/archiso-tmp -o out .
+
